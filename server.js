17fb13a3fea60166b2c0512e51724e13f5a4c1f7
VemulaSaiTharunGoud <vsaitharun21@gmail.com>
2025-11-21 20:40:23 +0530

    changed to normal azure task ci/cd

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello from Azure DevOps!! Good Morning');
+  res.end('Hello from Azure DevOps! This is a CI/CD Pipeine Task');
 });
 server.listen(port, () => console.log(`Server running on ${port}`));
