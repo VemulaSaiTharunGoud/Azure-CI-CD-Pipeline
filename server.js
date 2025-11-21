6770132407a5b7b85ef4f1d34772506287a05f05
VemulaSaiTharunGoud <vsaitharun21@gmail.com>
2025-11-21 17:27:25 +0530

    server123ereg

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello from Azure DevOps demo CI/CD Pipeline');
+  res.end('Hello from Azure DevOps demo CI/CD Pipeline Assignment.in');
 });
 server.listen(port, () => console.log(`Server running on ${port}`));
